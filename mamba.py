7cc2814495c9c77d4ece550980c80d8f2b2e875d
Özgür Soysal <120128014+factoreminv@users.noreply.github.com>
2024-04-23 23:01:51 +0300

    Update mamba.py

--- mamba.py
+++ mamba.py
@@ -1,9 +1,10 @@
 import math
 from dataclasses import dataclass
 from typing import Union
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from pscan import pscan
+
+class Mamba:
+  def __init__:
